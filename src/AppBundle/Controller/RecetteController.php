faf0051d1c57b9baf326d966166a1932d21b491f
mystiick <polmickael3@gmail.com>
2019-05-11 23:38:24 +0200

    modify path to turbolinks take the dependencies and remove right for see the recipe if not logged at the app

--- src/AppBundle/Controller/RecetteController.php
+++ src/AppBundle/Controller/RecetteController.php
@@ -14,7 +14,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  * Planning controller.
  *
  * @Route("recette")
- * @Security("has_role('ROLE_USER')")
  */
 class RecetteController extends Controller
 {
